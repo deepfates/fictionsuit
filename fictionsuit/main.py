c383e664271f104b2411a71cb0d678735d387e58
John Malmberg <john.i.malmberg@gmail.com>
2023-03-19 14:54:55 -0500

    Wrap discord messages in a platform-agnostic UserMessage

--- fictionsuit/main.py
+++ fictionsuit/main.py
@@ -1,9 +1,12 @@
-import config
 import discord
+
 from bot import Bot
+import config
 
 from commands.command_group import command_split
 from commands.basics import Basics
+
+from api_wrap.discord import DiscordMessage
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,21 +19,22 @@ async def on_message(message):
         # Don't self-reply
         return
    
-    prefix_lower = config.COMMAND_PREFIX.lower()
-    if not message.content.lower().startswith(prefix_lower):
+    wrap = DiscordMessage(message)
+
+    if not wrap.has_prefix(config.COMMAND_PREFIX):
         return # Not handling non-command messages, for now
 
-    (cmd, args) = command_split(message.content, config.COMMAND_PREFIX)
+    (cmd, args) = command_split(wrap.content, config.COMMAND_PREFIX)
 
     if cmd is None:
         return # Prefix, but no command. Nothing to do.
 
     for group in command_groups:
-        if await group.handle(message, cmd, args):
+        if await group.handle(wrap, cmd, args):
             return
 
     if cmd == 'help':
-        await message.channel.send(f'Sorry, there\'s no command called "{args}"')
+        await wrap.reply(f'Sorry, there\'s no command called "{args}"')
     
 
 def main():
