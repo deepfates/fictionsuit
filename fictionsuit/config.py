12c1d0885bb767063702e731f07d38470c723788
John Malmberg <john.i.malmberg@gmail.com>
2023-03-23 20:31:00 -0500

    Make supabase upload optional

--- fictionsuit/config.py
+++ fictionsuit/config.py
@@ -29,4 +29,4 @@ SUMMARIZE_MSG = os.getenv("SUMMARIZE_MSG", "Summarize the following text:")
 # Supabase
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
-UPLOAD_TO_SUPABASE = True
+UPLOAD_TO_SUPABASE = os.getenv("UPLOAD_TO_SUPABASE", False)
